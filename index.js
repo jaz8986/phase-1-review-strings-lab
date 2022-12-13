4dc6201a42a8858e8aa9c8dd0d8a5778e39c05c8
Jaskiran Singh <jsingh8986@yahoo.com>
2022-12-13 00:23:50 -0800

    commit tree 053680b402d1315731671d065b3dcd0f2b10b4a2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "jsingh8986"
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+const shortGreeting = "Welcome, " + currentUser.substring(0,1) + "!";
